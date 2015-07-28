cacfb62d58608619a80936e97a71622d07446a25
lock0 <xiaoxiao95888@163.com>
2015-07-28 21:00:08 +0800

    Update controller.js

--- HelloAngularjs/Scripts/controllers/controller.js
+++ HelloAngularjs/Scripts/controllers/controller.js
@@ -2,6 +2,6 @@
 
 phonecatApp.controller('PhoneListCtrl', function ($scope,$http) {    
     $http.get('/api/phone/').success(function (data, status, headers, config) {
-        $scope.phones = data;
+        $scope.phones = data;     
     })
 });
